ff0537ac8903f8eeb84ed67d3e9acb94b6ce497c
JohnMushatt <john.mushatt@gmail.com>
2018-11-29 18:37:49 -0500

    Updated lambda function

--- src/test/java/com/amazonaws/lambda/demo/LambdaFunctionHandlerTest.java
+++ src/test/java/com/amazonaws/lambda/demo/LambdaFunctionHandlerTest.java
@@ -6,29 +6,49 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.gson.JsonObject;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
 public class LambdaFunctionHandlerTest {
-
-    private static final String SAMPLE_INPUT_STRING = "{\"foo\": \"bar\"}";
-    private static final String EXPECTED_OUTPUT_STRING = "{\"FOO\": \"BAR\"}";
+	Context createContext(String apiCall) {
+		TestContext ctx = new TestContext();
+		ctx.setFunctionName(apiCall);
+		return ctx;
+	}
 
     @Test
-    public void testLambdaFunctionHandler() throws IOException {
-        LambdaFunctionHandler handler = new LambdaFunctionHandler();
+    public void testCreateCalender() throws IOException {
+        CreateScheduleHandler handler = new CreateScheduleHandler();
+        JsonObject test = new JsonObject();
+        String initDate = "2020-10-10";
+        String initTime = "06:21:22";
+        String startDate = "2020-10-10";
+        String endDate = "2020-10-11";
+        String startTime = "08:00";
+        String endTime = "11:00";
+        int tsDuration = 20;
+        test.addProperty("initDate", initDate);
+        test.addProperty("initTime", initTime);
+        test.addProperty("startDate", startDate);
+        test.addProperty("endDate", endDate);
+        test.addProperty("startTime", startTime);
+        test.addProperty("endTime", endTime);
+        test.addProperty("tsDuration", tsDuration);
+        JsonObject body = new JsonObject();
+        body.addProperty("body", test.toString());
 
-        InputStream input = new ByteArrayInputStream(SAMPLE_INPUT_STRING.getBytes());;
+        String jsonRequest = body.toString();
+
+        InputStream input = new ByteArrayInputStream(jsonRequest.getBytes());
+
         OutputStream output = new ByteArrayOutputStream();
 
-        handler.handleRequest(input, output, null);
 
-        // TODO: validate output here if needed.
-        String sampleOutputString = output.toString();
-        System.out.println(sampleOutputString);
-        Assert.assertEquals(EXPECTED_OUTPUT_STRING, sampleOutputString);
+        handler.handleRequest(input, output, createContext(""));
     }
 }
