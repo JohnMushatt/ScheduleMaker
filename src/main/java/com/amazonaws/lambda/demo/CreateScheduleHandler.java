6cd65491ffd223fe9a7ce886b1629e89ee46ce03
JohnMushatt <john.mushatt@gmail.com>
2018-11-29 21:21:27 -0500

    Added lambda

--- src/main/java/com/amazonaws/lambda/demo/CreateScheduleHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/CreateScheduleHandler.java
@@ -1,4 +1,5 @@
 package com.amazonaws.lambda.demo;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,52 +21,49 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.google.gson.Gson;
 
-
 public class CreateScheduleHandler implements RequestStreamHandler {
 
 	public LambdaLogger logger = null;
 
-	boolean createSchedule(Date initDate, Time initTime,
-			Date startDate,Date endDate,String startTime, String endTime,
+	boolean createSchedule(Date initDate, Time initTime, Date startDate, Date endDate, String startTime, String endTime,
 			int tsDuration) throws Exception {
-		if(logger !=null) {
+		if (logger != null) {
 			logger.log("in createSchedule");
 		}
 		SchedulesDAO dao = new SchedulesDAO();
 		Random r = new Random();
-		String organizerId = "" + (int) (r.nextDouble()*10000);
+		String organizerId = "" + (int) (r.nextDouble() * 10000);
 		String sId = initDate + organizerId + initTime;
-		String secretCode = sId+organizerId;
+		String secretCode = sId + organizerId;
 		Schedule exist = dao.getSchedule(sId);
-		Schedule schedule = new Schedule(sId,initDate,initTime,organizerId, startDate,
-				endDate,startTime,endTime, tsDuration, secretCode);
-		if(exist ==null) {
+		Schedule schedule = new Schedule(sId, initDate, initTime, organizerId, startDate, endDate, startTime, endTime,
+				tsDuration, secretCode);
+		if (exist == null) {
 			return dao.addSchedule(schedule);
-		}
-		else {
+		} else {
 			return dao.updateSchedule(schedule);
 		}
 	}
-    @Override
-    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
 
-    	logger = context.getLogger();
-    	logger.log("Loading Java Lambda handler to create schedule");
+	@Override
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
 
-    	JSONObject headerJson = new JSONObject();
-    	headerJson.put("Content-Type", "application/json");
-    	headerJson.put("Access-Control-Allow-Methods", "get,Post,OPTIONS");
-    	headerJson.put("Access-Control-Allow-Origin", "*");
+		logger = context.getLogger();
+		logger.log("Loading Java Lambda handler to create schedule");
 
-    	JSONObject responseJson = new JSONObject();
-    	responseJson.put("headers", headerJson);
-    	CreateScheduleResponse response = null;
+		JSONObject headerJson = new JSONObject();
+		headerJson.put("Content-Type", "application/json");
+		headerJson.put("Access-Control-Allow-Methods", "GET,Post,OPTIONS");
+		headerJson.put("Access-Control-Allow-Origin", "*");
 
-    	String body;
+		JSONObject responseJson = new JSONObject();
+		responseJson.put("headers", headerJson);
+		CreateScheduleResponse response = null;
 
-    	boolean processed =false;
-    	try {
-    		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		String body;
+		boolean processed = false;
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 			JSONParser parser = new JSONParser();
 			JSONObject event = (JSONObject) parser.parse(reader);
 			logger.log("event:" + event.toJSONString());
@@ -73,48 +71,50 @@ public class CreateScheduleHandler implements RequestStreamHandler {
 			String method = (String) event.get("httpMethod");
 			if (method != null && method.equalsIgnoreCase("OPTIONS")) {
 				logger.log("Options request");
-				response = new CreateScheduleResponse("name", 200);  // OPTIONS needs a 200 response
-		        responseJson.put("body", new Gson().toJson(response));
-		        processed = true;
-		        body = null;
+				response = new CreateScheduleResponse("name", 200); // OPTIONS needs a 200 response
+				responseJson.put("body", new Gson().toJson(response));
+				processed = true;
+				body = null;
 			} else {
-				body = (String)event.get("body");
+				body = (String) event.get("body");
 				if (body == null) {
-					body = event.toJSONString();  // this is only here to make testing easier
+					body = event.toJSONString(); // this is only here to make testing easier
 				}
 			}
 
-    	}
-    	catch(ParseException pe) {
-    		logger.log(pe.toString());
-			response = new CreateScheduleResponse("Bad Request:" + pe.getMessage(), 422);  // unable to process input
-	        responseJson.put("body", new Gson().toJson(response));
-	        processed = true;
-	        body = null;
-    	}
+		} catch (ParseException pe) {
+			logger.log(pe.toString());
+			response = new CreateScheduleResponse("Bad Request:" + pe.getMessage(), 422); // unable to process input
+			responseJson.put("body", new Gson().toJson(response));
+			processed = true;
+			body = null;
+		}
 
-    	if(!processed) {
-    		CreateScheduleRequest req = new Gson().fromJson(body, CreateScheduleRequest.class);
-    		logger.log(req.toString());
+		if (!processed) {
+			CreateScheduleRequest req = new Gson().fromJson(body, CreateScheduleRequest.class);
+			logger.log(req.toString());
 
-    		CreateScheduleResponse resp;
+			CreateScheduleResponse resp;
 
-    		try {
-    			if(createSchedule(req.initDate,req.initTime,req.startDate,req.endDate,
-    					req.startTime,req.endTime,req.tsDuration));
-    		}
-    		catch(Exception e) {
-    			resp = new CreateScheduleResponse("Unable to create schedule: " + req.initDate);
-    		}
+			try {
+				if (createSchedule(req.initDate, req.initTime, req.startDate, req.endDate, req.startTime, req.endTime,
+						req.tsDuration)) {
+					resp = new CreateScheduleResponse("(" + req.startDate + "," + req.endDate + ")", 200);
+				}
+				else {
+					resp = new CreateScheduleResponse("Failed to create schedule",403);
+				}
+			} catch (Exception e) {
+				resp = new CreateScheduleResponse("Unable to create schedule: " + req.initDate,403);
+			}
+			responseJson.put("body", new Gson().toJson(resp));
 
-
-    	}
-    	  logger.log("end result:" + responseJson.toJSONString());
-          logger.log(responseJson.toJSONString());
-          OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8");
-          writer.write(responseJson.toJSONString());
-          System.out.println("here");
-          writer.close();
-    }
+		}
+		logger.log("end result:" + responseJson.toJSONString());
+		logger.log(responseJson.toJSONString());
+		OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8");
+		writer.write(responseJson.toJSONString());
+		writer.close();
+	}
 
 }
