2e2f88308431115c54f551dac27d9e4645a04a22
JohnMushatt <john.mushatt@gmail.com>
2018-12-03 22:46:33 -0500

    Updated CreateMeetingResponse/Request

--- src/main/java/com/amazonaws/lambda/demo/CreateMeetingResponse.java
+++ src/main/java/com/amazonaws/lambda/demo/CreateMeetingResponse.java
@@ -3,7 +3,11 @@ package com.amazonaws.lambda.demo;
 public class CreateMeetingResponse {
 	String body;
 	int code;
-
+	/**
+	 * Create HTTP meeting response
+	 * @param body JSON body
+	 * @param code HTTP success/failure code
+	 */
 	public CreateMeetingResponse(String body, int code) {
 		this.body=body;
 		this.code=code;
